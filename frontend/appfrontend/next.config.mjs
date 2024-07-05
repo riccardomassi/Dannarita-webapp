8d0c3d268ccfb457e0b67ee44113fa87cb58d2dc
Riccardo Massi <rickymassi@gmail.com>
2024-07-05 21:13:59 +0200

    piccole modifiche

--- frontend/appfrontend/next.config.mjs
+++ frontend/appfrontend/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dannarita-backend.serveo.net',
+        // change the path to protocol and hostname if you are using a local Django server
+        // protocol: 'http',
+        // hostname: '127.0.0.1',
+      }
+    ],
+  },
+};
 
 export default nextConfig;
