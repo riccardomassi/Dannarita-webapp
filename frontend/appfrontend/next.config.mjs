8497745b2fd6654d894d3c2a4a166fcbce8ab19e
Riccardo Massi <rickymassi@gmail.com>
2024-07-05 20:48:30 +0200

    prova backend usando serveo.net

--- frontend/appfrontend/next.config.mjs
+++ frontend/appfrontend/next.config.mjs
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*.serveo.net'
-      },
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1'
-      }
-    ],
+    loader: 'imgix', // Choose your preferred loader
+    path: 'https://*.serveo.net/', // Replace with your serveo.net backend URL
+    // path : 'http://127.0.0.1:8000/',
+    // change the path to http://127.0.0.1:8000/ if you are using a local Django server
   },
+
 };
 
 export default nextConfig;
